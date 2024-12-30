5191ebfc9ec5bb32c2eb731e298abfe4c8b4f192
Noran Salah <noransalah840@gmail.com>
2024-12-30 21:15:28 +0200

    Q1

--- Assignment5/Program.cs
+++ Assignment5/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Question 01
+            //1.Create an enum called "WeekDays" with the days of the week(Monday to Sunday) as its members.Then, write a C# program that prints out all the days of the week using this enum.
+
+            //public enum WeekDays
+            //{
+            //   Monday , 
+            //   Tuesday ,
+            //   Wenesday,
+            //   Thursday ,
+            //   Friday ,
+            //   Saturday ,
+            //   Sunday
+            //}
+            #endregion
+
         }
     }
 }
